0625ab6a618ad55a8abd60c81d2bcc9ad35fe12f
Ilya Shaisultanov <ilya.shaisultanov@gmail.com>
2019-02-17 10:40:19 -0800

    Working, finally

--- run.sh
+++ run.sh
@@ -1,25 +1,35 @@
 #!/bin/bash
 
-WORKDIR=/tmp
-ZIPBALL_URL="https://github.com/diversario/ansible-macos-init/archive/master.zip"
-ZIPBALL_FILE=ansible-macos-init.zip
+read -p "[IMPORTANT] Pre-requisites:
 
-echo 'Enter credentials for installation'
+- Sign in into App Store to install apps
+- Add Terminal.app to System Preferences -> Security & Privacy -> Full Disk Access to allow it to modify all defaults (this will restart Terminal)
 
-read -s -p 'sudo password: ' SUDO_PASSWORD; echo ''
-read -s -p 'Github access token: ' GITHUB_ACCESS_TOKEN; echo ''
-read -p 'Apple ID email: ' MAS_EMAIL
-read -s -p 'Apple ID password: ' MAS_PASSWORD; echo ''
+Press any key to continue
+"
+
+read -p "Sign in into App Store and press any key to continue"
+
+# get credentials for stuff right away
+if [[ -z $SUDO_PASSWORD ]]; then
+  read -s -p 'sudo password: ' SUDO_PASSWORD; echo ''
+fi
+
+if [[ -z $GITHUB_ACCESS_TOKEN ]]; then
+  read -s -p 'Github access token: ' GITHUB_ACCESS_TOKEN; echo ''
+fi
 
 export SUDO_PASSWORD
 export GITHUB_ACCESS_TOKEN
-export MAS_EMAIL
-export MAS_PASSWORD
 
 if [[ ! -f $(which brew) ]]; then
   CI=1 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   brew install ansible
 fi
+
+WORKDIR=/tmp
+ZIPBALL_URL="https://github.com/diversario/ansible-macos-init/archive/master.zip"
+ZIPBALL_FILE=ansible-macos-init.zip
 
 curl -fsSL $ZIPBALL_URL -o $WORKDIR/$ZIPBALL_FILE
 cd $WORKDIR
