a54137692e929960f9d9db46f0f1e516d2957537
Ilya Shaisultanov <ilya.shaisultanov@gmail.com>
2019-02-16 19:35:17 -0800

    WIP

---
+++ README.md
@@ -0,0 +1,5 @@
+# Run
+
+```sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/diversario/ansible-macos-init/master/run.sh)"
+```
--- run.sh
+++ run.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
-CI=1 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+WORKDIR=/tmp
+ZIPBALL_URL="https://github.com/diversario/ansible-macos-init/archive/master.zip"
+ZIPBALL_FILE=ansible-macos-init.zip
 
-brew install ansible
+if [[ ! -f $(which brew) ]]; then
+  CI=1 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  brew install ansible
+fi
+
+curl -fsSL $ZIPBALL_URL -o $WORKDIR/$ZIPBALL_FILE
+cd $WORKDIR
+unzip -o $ZIPBALL_FILE
+cd ansible-macos-init-master
+
+ansible-galaxy install -r requirements.yaml
+ansible-playbook -i hosts.yaml setup-mac.yaml
