fc9c983efdda708d9b4e5ed5560f3f0b99f63403
Ilya Shaisultanov <ilya.shaisultanov@gmail.com>
2019-02-16 21:39:00 -0800

    Remove unsupported module

--- run.sh
+++ run.sh
@@ -4,6 +4,18 @@ WORKDIR=/tmp
 ZIPBALL_URL="https://github.com/diversario/ansible-macos-init/archive/master.zip"
 ZIPBALL_FILE=ansible-macos-init.zip
 
+echo 'Enter credentials for installation'
+
+read -s -p 'sudo password: ' SUDO_PASSWORD; echo ''
+read -s -p 'Github access token: ' GITHUB_ACCESS_TOKEN; echo ''
+read -p 'Apple ID email: ' MAS_EMAIL
+read -s -p 'Apple ID password: ' MAS_PASSWORD; echo ''
+
+export SUDO_PASSWORD
+export GITHUB_ACCESS_TOKEN
+export MAS_EMAIL
+export MAS_PASSWORD
+
 if [[ ! -f $(which brew) ]]; then
   CI=1 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
   brew install ansible
