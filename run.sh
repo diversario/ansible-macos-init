71803a48c53b2356781b53e6aad6cd6c8e3bf07f
Ilya Shaisultanov <ilya.shaisultanov@gmail.com>
2019-02-17 17:56:57 -0800

    Do temp passwordless sudo

--- run.sh
+++ run.sh
@@ -8,16 +8,29 @@ read -p "[IMPORTANT] Pre-requisites:
 Press any key to continue
 "
 
-# get credentials for stuff right away
-if [[ -z $SUDO_PASSWORD ]]; then
-  read -s -p 'sudo password: ' SUDO_PASSWORD; echo ''
+TEMP_SUDOERS_FILE=/etc/sudoers.d/ansible-passwordless-sudo
+
+function cleanup() {
+  sudo rm $TEMP_SUDOERS_FILE
+}
+
+trap cleanup SIGINT SIGTERM ERR EXIT
+
+echo "Passwordless sudo will be enabled for the duration of the install"
+echo '%admin ALL = (ALL) NOPASSWD: ALL' | sudo tee $TEMP_SUDOERS_FILE
+
+# If this user's login shell is not already "zsh", attempt to switch.
+echo "Switching shell to zsh now to avoid a password prompt later"
+TEST_CURRENT_SHELL=$(basename "$SHELL")
+if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
+  chsh -s $(grep /zsh$ /etc/shells | tail -1) || true
 fi
 
 if [[ -z $GITHUB_ACCESS_TOKEN ]]; then
   read -s -p 'Github access token: ' GITHUB_ACCESS_TOKEN; echo ''
 fi
 
-export SUDO_PASSWORD
+# export SUDO_PASSWORD
 export GITHUB_ACCESS_TOKEN
 
 if [[ ! -f $(which brew) ]]; then
@@ -35,4 +48,9 @@ unzip -o $ZIPBALL_FILE
 cd ansible-macos-init-master
 
 ansible-galaxy install -r requirements.yaml
-ansible-playbook -i hosts.yaml setup-mac.yaml
+
+if [[ -z SKIP_MAS ]]; then
+  ansible-playbook -i hosts.yaml setup-mac.yaml
+else
+  ansible-playbook -i hosts.yaml setup-mac.yaml --extra-vars '{"mas_installed_apps":[]}'
+fi
